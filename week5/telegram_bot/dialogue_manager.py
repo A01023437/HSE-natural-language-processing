adf21b178871cf69e72ea0182d5b11f30c52b4b3
a01023437 <a01023437@itesm.mx>
2020-07-18 17:42:53 -0500

    manager

--- week5/telegram_bot/dialogue_manager.py
+++ week5/telegram_bot/dialogue_manager.py
@@ -45,7 +45,8 @@ class DialogueManager(object):
         self.tag_classifier = unpickle_file(paths['TAG_CLASSIFIER'])
         self.thread_ranker = ThreadRanker(paths)
 
-        self.chitchat_bot = None
+        self.create_chitchat_bot()
+
 
     def create_chitchat_bot(self):
         """Initializes self.chitchat_bot with some conversational model."""
